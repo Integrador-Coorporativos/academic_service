b43e1b52d4e943a4711ae5f945e1f8f60bd22f20
ferreira.lima1 <ferreira.lima1@escolar.ifrn.edu.br>
2025-11-24 16:20:54 -0300

    first commit

---
+++ src/main/java/br/com/ifrn/EvaluationsService/evaluations_service/EvaluationsServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifrn.EvaluationsService.evaluations_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifrn/EvaluationsService/evaluations_service/EvaluationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifrn.EvaluationsService.evaluations_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
